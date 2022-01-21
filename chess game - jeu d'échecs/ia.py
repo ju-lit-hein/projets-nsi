4fc64c0457acb83b881de179e766652bd5f96873
ju-lit-hein <ferd.julien@gmail.com>
2022-01-21 13:09:24 +0400

    Update ia.py

--- chess game - jeu d'échecs/ia.py
+++ chess game - jeu d'échecs/ia.py
@@ -25,9 +25,32 @@ class IA():
 
 # l'algorithme minimax utilisera le principe de récursivité
 
-def minimax(depth, score):
-    assert type(score) == dict, "score n'est pas un dictionnaire"
-    if depth:
-        for dpd in score:
-            pass
+def minimax(data={}, miniormaxi=-1):
+    '''
+    
+    miniormaxi : True si on cherche le et False si on cherche le
+    '''
+    assert type(data) == dict, "L'argument doit être un dictionnaire"
+    if len(data) % 2 == 0:
+        depth = len(data) // 2
+    else:
+        depth = len(data) // 2 + 1
+    if miniormaxi == -1:                                                # valeur de 1- par défaut pour pouvoir définir True ou False selon depth qui n'est pas encore définie lorsqu'on appelle la focntion
+        miniormaxi = bool(depth % 2)
+    if depth != 0:
+        erkjvherk = {}
+        for element in range(depth):
+            try:
+                pass
+            except:
+                pass
+
+### comparer deux par deux les élément et ajouter le mini ou maxi dans le dico erkvherk                
+
+
+
+
+
+
 ### voir si c'est pas mieux d'utiliser une liste [key, value, ...]
+a = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10}
